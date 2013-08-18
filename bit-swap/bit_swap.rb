96facc7b49b88613835736799f5340f3e5373f1f
TH <tylerhartland7@gmail.com>
2013-08-18 11:00:36 -0500

    refactor for clarity

--- bit-swap/bit_swap.rb
+++ bit-swap/bit_swap.rb
@@ -1,11 +1,10 @@
 def swap_bits(int, i1, i2)
   return int if int[i1] == int[i2]
   if int[i1] == 1
-    int = int - bit_value(i1) + bit_value(i2)
+    int - bit_value(i1) + bit_value(i2)
   else
-    int = int + bit_value(i1) - bit_value(i2)
+    int + bit_value(i1) - bit_value(i2)
   end
-  int
 end
 
 def bit_value(i)
